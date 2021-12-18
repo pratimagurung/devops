c6729af57a4f30fbc5d3aaad4a17945aaa368322
pratimagurung <pratimagurun589@gmail.com>
2021-12-18 14:52:17 +0545

    port changed

--- src/main/java/com/pratima/practice/model/User.java
+++ src/main/java/com/pratima/practice/model/User.java
@@ -1,32 +1,32 @@
-package com.pratima.practice.model;
-
-import java.util.List;
-
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.OneToMany;
-
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
-@Entity
-@NoArgsConstructor
-@AllArgsConstructor
-@Getter @Setter
-public class User {
-	@Id
-	private int id;
-	private String username;
-	private String password;
-	private String role;
-	private boolean enabled;
-	
-@OneToMany(mappedBy = "user")
-private List<Role> roleId;
-
-}
+//package com.pratima.practice.model;
+//
+//import java.util.List;
+//
+//import javax.persistence.Entity;
+//import javax.persistence.Id;
+//import javax.persistence.JoinColumn;
+//import javax.persistence.OneToMany;
+//
+//import org.springframework.boot.autoconfigure.domain.EntityScan;
+//
+//import lombok.AllArgsConstructor;
+//import lombok.Getter;
+//import lombok.NoArgsConstructor;
+//import lombok.Setter;
+//
+////@Entity
+//@NoArgsConstructor
+//@AllArgsConstructor
+//@Getter @Setter
+//public class User {
+////	@Id
+//	private int id;
+//	private String username;
+//	private String password;
+//	private String role;
+//	private boolean enabled;
+//	
+////@OneToMany(mappedBy = "user")
+//private List<Role> roleId;
+//
+//}
